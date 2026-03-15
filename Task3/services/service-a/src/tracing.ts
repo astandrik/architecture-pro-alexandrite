e7e6583175838c59583ca5c4e7c12690fb72a4df
Anton Standrik <astandrik@yandex-team.ru>
2026-03-15 18:41:46 +0300

    feat: task1

---
+++ Task3/services/service-a/src/tracing.ts
@@ -0,0 +1,35 @@
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+
+const traceExporter = new OTLPTraceExporter({
+  url:
+    process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT ??
+    "http://localhost:4318/v1/traces",
+});
+
+const sdk = new NodeSDK({
+  traceExporter,
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      "@opentelemetry/instrumentation-fs": {
+        enabled: false,
+      },
+    }),
+  ],
+});
+
+sdk.start();
+
+const shutdown = async () => {
+  await sdk.shutdown().catch(() => undefined);
+  process.exit(0);
+};
+
+process.once("SIGINT", () => {
+  void shutdown();
+});
+
+process.once("SIGTERM", () => {
+  void shutdown();
+});
